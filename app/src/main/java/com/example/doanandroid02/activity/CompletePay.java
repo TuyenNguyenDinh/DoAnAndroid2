bf3c291df7ac61696dfd8583f280ac42def21ad8
TuyenNguyenDinh <tuyennguyendinh1608@gmail.com>
2021-02-02 09:25:56 +0700

    á

---
+++ app/src/main/java/com/example/doanandroid02/activity/CompletePay.java
@@ -0,0 +1,14 @@
+package com.example.doanandroid02.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CompletePay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_complete_pay);
+    }
+}
---
+++ app/src/main/java/com/example/doanandroid02/adapter/SearchAdapter.java
@@ -0,0 +1,4 @@
+package com.example.doanandroid02.adapter;
+
+public class SearchAdapter {
+}
